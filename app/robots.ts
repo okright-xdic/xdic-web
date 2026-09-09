1986a50a982c5733a95322edfa65fadfd4e63387
Taehoon Jang <zzangth@gmail.com>
2026-09-09 16:51:22 +0900

    Allow app-ads.txt crawler

--- app/robots.ts
+++ app/robots.ts
@@ -2,14 +2,24 @@ import type { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: [
-        '/app',
-        '/admin-write',
-      ],
-    },
+    rules: [
+      {
+        userAgent: 'Google-adstxt',
+        allow: '/app-ads.txt',
+      },
+      {
+        userAgent: '*',
+        allow: [
+          '/',
+          '/ads.txt',
+          '/app-ads.txt',
+        ],
+        disallow: [
+          '/app',
+          '/admin-write',
+        ],
+      },
+    ],
     sitemap: 'https://www.x-dic.com/sitemap.xml',
   };
 }
